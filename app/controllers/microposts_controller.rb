2e31b0ee6324614ebc61c1e13c796953e9f7d8fb
Joe Coney <j.n.coney@gmail.com>
2014-12-24 16:11:29 +0000

    Add user microposts

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -22,7 +22,7 @@ class MicropostsController < ApplicationController
   private
 
   def micropost_params
-      params.require(:micropost).permit(:title, :content, :picture)
+      params.require(:micropost).permit(:title, :content)
     end
 
   def correct_user
